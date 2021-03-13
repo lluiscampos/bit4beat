b944544b86424cb9b3c7f5a056c8829c83e8d0f0
Lluís M. Campos Martínez <lluis@tastaollet.es>
2021-03-13 18:14:31 +0100

    model/record: Add fields validation
    Signed-off-by: Lluís M. Campos Martínez <lluis@tastaollet.es>

--- back/model/record.go
+++ back/model/record.go
@@ -1,28 +1,52 @@
 package model
 
+import (
+	"errors"
+	"fmt"
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var (
+	distanceReg = regexp.MustCompile(`^[-0-9]+ (km|m)$`)
+)
+
 type Record struct {
 	// Auto-generated on insert
-	ID string `json:"id,omitempty"`
+	ID string `json:"id" validate:"required"`
 
-	// Kind
-	Kind string `json:"kind"`
+	// One of ski, hike, bike
+	Kind string `json:"kind" validate:"required,eq=ski|eq=hike|eq=bike"`
 
-	// Date, any string will do for now
-	Date string `json:"date"`
+	// Any string will do for now
+	Date string `json:"date" validate:"required"`
 
-	Place string `json:"place"`
+	// Any string
+	Place string `json:"place" validate:"required"`
 
 	// Accepts .5 precision, suffixed with unit
 	Distance string `json:"distance,omitempty"`
 
 	Participants []string `json:"participants,omitempty`
 
-	Reference string `json:"reference"`
+	Reference string `json:"reference,omitempty"`
 }
 
 func (r Record) Validate() error {
-	// TODO: Check Kind
-	// TODO: Check Distance
-	// ...
+	v := validator.New()
+	err := v.Struct(r)
+	if err != nil {
+		return err
+	}
+
+	// Custom validation for Distance
+	if r.Distance != "" {
+		match := distanceReg.MatchString(r.Distance)
+		if !match {
+			return errors.New(fmt.Sprintf("invalid Distance format in %q", r.Distance))
+		}
+	}
+
 	return nil
 }
---
+++ back/model/record_test.go
@@ -0,0 +1,87 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func newRecordMinValid() *Record {
+	return &Record{
+		ID:    "stuff-id",
+		Kind:  "ski",
+		Date:  "sometime",
+		Place: "somewhere",
+	}
+}
+
+func TestRequiredFields(t *testing.T) {
+	rValid := newRecordMinValid()
+	err := rValid.Validate()
+	assert.NoError(t, err)
+
+	rMissingID := newRecordMinValid()
+	rMissingID.ID = ""
+	err = rMissingID.Validate()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "'ID' failed on the 'required' tag")
+
+	rMissingKind := newRecordMinValid()
+	rMissingKind.Kind = ""
+	err = rMissingKind.Validate()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "'Kind' failed on the 'required' tag")
+
+	rMissingDate := newRecordMinValid()
+	rMissingDate.Date = ""
+	err = rMissingDate.Validate()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "'Date' failed on the 'required' tag")
+
+	rMissingPlace := newRecordMinValid()
+	rMissingPlace.Place = ""
+	err = rMissingPlace.Validate()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "'Place' failed on the 'required' tag")
+}
+
+func TestValidateKind(t *testing.T) {
+	rInvalidKind := newRecordMinValid()
+	rInvalidKind.Kind = "invalid"
+
+	err := rInvalidKind.Validate()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "Field validation for 'Kind' failed")
+}
+
+func TestValidateDistance(t *testing.T) {
+	validDistances := []string{
+		"123 km",
+		"1 m",
+		"-1 km",
+		"987834726483217468127341827987 km",
+	}
+	for _, d := range validDistances {
+		rValidDistance := newRecordMinValid()
+		rValidDistance.Distance = d
+
+		err := rValidDistance.Validate()
+		assert.NoError(t, err)
+	}
+
+	invalidDistances := []string{
+		"123 somethingelse",
+		" 123 m",
+		"123 m ",
+		"+123 m",
+		"m m",
+	}
+	for _, d := range invalidDistances {
+		rInvalidDistance := newRecordMinValid()
+		rInvalidDistance.Distance = d
+
+		err := rInvalidDistance.Validate()
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "invalid Distance format")
+	}
+}
