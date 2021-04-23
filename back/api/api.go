f5eef6ddca57ec3987b9fb2e40173456fbde34bb
Lluís M. Campos Martínez <lluis@tastaollet.es>
2021-04-23 15:59:46 +0200

    api/api.go: move logic to separate methods
    Signed-off-by: Lluís M. Campos Martínez <lluis@tastaollet.es>

--- back/api/api.go
+++ back/api/api.go
@@ -20,45 +20,51 @@ func NewApi(store store.Store) *Api {
 	}
 }
 
+func (api *Api) createRecordHandler(context *gin.Context) {
+	jsonData, err := ioutil.ReadAll(context.Request.Body)
+	if err != nil {
+		context.JSON(500, gin.H{"error": string(err.Error())})
+	}
+
+	var record model.Record
+	err = json.Unmarshal(jsonData, &record)
+	if err != nil {
+		context.JSON(500, gin.H{"error": string(err.Error())})
+	}
+
+	err = api.store.CreateRecord(&record)
+	if err != nil {
+		context.JSON(500, gin.H{"error": string(err.Error())})
+	}
+}
+
+func (api *Api) showRecordHandler(context *gin.Context) {
+	id := context.Param("id")
+	record, err := api.store.ReadRecord(id)
+	if err != nil {
+		context.JSON(500, gin.H{"error": string(err.Error())})
+	} else {
+		context.JSON(200, record)
+	}
+}
+
+func (api *Api) listRecordsHandler(context *gin.Context) {
+	records, err := api.store.ListRecords()
+	if err != nil {
+		context.JSON(500, gin.H{"error": string(err.Error())})
+	} else {
+		context.JSON(200, records)
+	}
+}
+
 func (api *Api) Serve() {
 	router := gin.Default()
 
-	router.POST("/record", func(context *gin.Context) {
-		jsonData, err := ioutil.ReadAll(context.Request.Body)
-		if err != nil {
-			context.JSON(500, gin.H{"error": string(err.Error())})
-		}
+	router.POST("/record", api.createRecordHandler)
 
-		var record model.Record
-		err = json.Unmarshal(jsonData, &record)
-		if err != nil {
-			context.JSON(500, gin.H{"error": string(err.Error())})
-		}
+	router.GET("/record/:id", api.showRecordHandler)
 
-		err = api.store.CreateRecord(&record)
-		if err != nil {
-			context.JSON(500, gin.H{"error": string(err.Error())})
-		}
-	})
-
-	router.GET("/record/:id", func(context *gin.Context) {
-		id := context.Param("id")
-		record, err := api.store.ReadRecord(id)
-		if err != nil {
-			context.JSON(500, gin.H{"error": string(err.Error())})
-		} else {
-			context.JSON(200, record)
-		}
-	})
-
-	router.GET("/records", func(context *gin.Context) {
-		records, err := api.store.ListRecords()
-		if err != nil {
-			context.JSON(500, nil)
-		} else {
-			context.JSON(200, records)
-		}
-	})
+	router.GET("/records", api.listRecordsHandler)
 
 	router.Run(":8080")
 }
